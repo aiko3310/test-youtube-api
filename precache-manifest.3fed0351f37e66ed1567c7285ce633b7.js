731257699ff57cc00e0a7ad572d82790016862a4
4N <aiko3310@hotmail.com>
2020-08-14 22:22:49 +0800

    Updates

---
+++ precache-manifest.3fed0351f37e66ed1567c7285ce633b7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c64ea36451e847de419443328c11631",
+    "url": "/test-youtube-api/index.html"
+  },
+  {
+    "revision": "d84258feccf240687f9b",
+    "url": "/test-youtube-api/static/css/2.d64cfc73.chunk.css"
+  },
+  {
+    "revision": "d84258feccf240687f9b",
+    "url": "/test-youtube-api/static/js/2.182a2549.chunk.js"
+  },
+  {
+    "revision": "dc027cd9760794d7c7da",
+    "url": "/test-youtube-api/static/js/main.a127b9f4.chunk.js"
+  },
+  {
+    "revision": "d3e287332972d7b1c4ae",
+    "url": "/test-youtube-api/static/js/runtime~main.80b231c7.js"
+  },
+  {
+    "revision": "311b7fc943f6469bbe77fe30e939cfc2",
+    "url": "/test-youtube-api/static/media/heart-regular.311b7fc9.svg"
+  },
+  {
+    "revision": "5dbbbe23e97832071f574844062f1117",
+    "url": "/test-youtube-api/static/media/heart-solid.5dbbbe23.svg"
+  },
+  {
+    "revision": "d9a24d05eed3db4d122adcaf0ac3d956",
+    "url": "/test-youtube-api/static/media/info.d9a24d05.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-youtube-api/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "47729526547fc45c3c9a9e26066c0c4d",
+    "url": "/test-youtube-api/static/media/nodata.47729526.gif"
+  },
+  {
+    "revision": "661a8c083717757bb10f3a762e5f096b",
+    "url": "/test-youtube-api/static/media/searchFail.661a8c08.gif"
+  }
+]);
